57ab92c1ba1289369ec20dda2aec591a979eba27
Eli Array Minkoff <eli@earrayminkoff.tech>
2023-07-27 20:16:37 -0400

    Fix main function signature for C programs

--- docs/test-programs/direct_syscall.c
+++ docs/test-programs/direct_syscall.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <sys/syscall.h> 
 
-int main(int argc, int argv[])
+int main(int argc, char** argv)
 {
     syscall(__NR_write, 1, "\x1b[H\x1b[J\x1b[3J", 10);
     syscall(__NR_exit, 0);
--- docs/test-programs/print_syscalls.c
+++ docs/test-programs/print_syscalls.c
@@ -2,7 +2,8 @@
 #include <sys/syscall.h> 
 #include <stdio.h>
 
-int main(int argc, int argv[])
+int main(int argc, char** argv)
 {
     printf("__NR_write: %d\n__NR_exit: %d\n", __NR_write, __NR_exit);
+    return(0);
 }
