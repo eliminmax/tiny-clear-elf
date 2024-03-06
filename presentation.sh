fb4f6e19ead8a0b1b6f71334bdb417ea924bb018
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-03-06 15:38:39 -0500

    Refactor presentation script

--- presentation.sh
+++ presentation.sh
@@ -1,136 +1,179 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # for this script to work, the system MUST be able to run foreign ELF binaries as
 # though they were native, using the Linux kernel's binfmt_misc capabilities and
-# something like qemu-user, and MUST have the following programs installed:
+# something like qemu-user, MUST be run from the Tiny Clear ELF git directory,
+# and MUST have the following programs installed:
 #
-# * du
-# * head
-# * tail
-# * cat
-# * figlet
-# * file
-# * hexyl
-# * readelf
 # * awk
-# * bash
-#
-# those are provided by the following packages on Debian-like systems:
-#
-# * coreutils (du, head, tail, cat)
+# * cat
+# * dirname
 # * figlet
-# * file
+# * head
 # * hexyl
+# * rasm2
+# * readelf
+# * realpath
+# * stty
+# * tail
+# * tput
+#
+# other than rasm2, those are provided by the following packages on Debian 12 (Bookworm):
+#
+# * awk (awk)
 # * binutils (readelf)
-# * gawk (awk)
-# * bash
+# * coreutils (cat, dirname, head, realpath, stty, tail)
+# * hexyl (hexyl)
+# * figlet (figlet)
+# * ncurses-bin (tput)
 #
-# It also depends on rasm2 (part of the radare2 reverse engineering toolkit), which is not in
-# Debian's repos. Its source can be fount at https://github.com/radareorg/radare2
+# awk is a virtual package provided by gawk, mawk, or original-awk.
+# The one awk command used here has been tested with all three
 #
-# This script must be run in a terminal emulator that's at least 112 columns and 56 rows in size.
+# rasm2 is part of the radare2 reverse engineering toolkit, which is not in
+# Debian's repos. Its source can be found at https://github.com/radareorg/radare2
+# 
+# Additionally, on a Debian system, to run foreign binaries as required, you must install:
+# * systemd or binfmt-support (enables binfmt_misc usage and provides an interface to it)
+# * qemu-user-binfmt or qemu-user-static (enables running foreign binaries)
+#
+# This script must be run in a terminal emulator that's at least 94 columns and 50 rows in size.
+# To avoid cutting off content, if it detects that it's not operating at that size, it will exit.
 
 # make sure we're in the right directory
-cd "$(dirname "$(realpath "$0")")" || exit 12
+cd "$(dirname "$(realpath "$0")")" || exit 1
 
-heading() {
-    printf '\e[%dH\e[1;4;32m(%s)\e[m\n' "$@"
+# save original terminal settings to be restored at the end
+stty_orig="$(stty -g)"
+
+stty -echo # hide anything the user types
+
+# https://old.reddit.com/r/linuxquestions/comments/rl1jai/comment/hphcj8h/
+tput civis # hide cursor
+
+show_heading() {
+    # display a heading at a specific row in bold green underlined, enclosed in parenthases
+    # first argument is the row number
+    # second argument is the heading text
+    printf '\e[%dH\e[1;4;32m(%s)\e[m\n' "$1" "$2"
 }
 
-bash_clear() {
-    # why not use one of the 9-10 different clear commands I'm presenting? I don't want to have to choose which one
-    # to use.
+sh_clear() {
+    # why not use one of the 10 different clear commands I'm presenting?
+    # I don't want to have to choose which one to use.
     printf '\e[H\e[J\e[3J'
 }
 
+cleanup() {
+    # at the end of a run, restore the terminal settings to how they were.
+    stty "$stty_orig"
+    tput cnorm
+    sh_clear
+}
+# call cleanup before quitting if common terminating signals are sent
+trap cleanup HUP INT QUIT ABRT TERM
+
+dimension_check () {
+    # method of getting rows and cols adapted from https://stackoverflow.com/a/68085512
+    term_size="$(stty size)"
+    cols="${term_size#* }"
+    rows="${term_size% *}"
+    if [ "$cols" -lt 94 ] || [ "$rows" -lt 50 ]; then
+        cleanup
+        printf 'stopping presentation - terminal too small.\n' >&2
+        exit 2
+    fi
+}
+dimension_check # run once right away
+trap dimension_check WINCH # re-run dimension_check if terminal is resized.
+
 arch_specific_logo() {
+    # display the Tiny Clear Elf ASCII-art logo, followed by the name
+    # in figlet italics
     cat logo
-    printf '\e[H'
-    figlet -f slant "$architecture" | sed $'s/^/\e[48G/'
+    printf '\e[H' # go to top row
+    # print the architecture name 48 cells in, so that it doesn't overwrite the logo.
+    figlet -f slant "$architecture" | awk '{printf "\x1b[48G%s\n", $0}'
 }
 
-wait_for_eof() {
-    cat >/dev/null
+wait_for_next() {
+    # hacky way to require presenter to press enter to continue
+    read -r _wait_var # this variable is unused, but needs to be set anyway
 }
 
-bash_clear
+sh_clear
 
 cat logo
 
-cat <<EOF
-$(printf '\e[1;32m')
+# the first line is an escape sequence to set the text to bold green
+printf '\e[1;32m
 | | -+- |
 +-+  |  |
 | | -+- .
-$(printf '\e[m')
-This is my presentation of the Tiny Clear Elf series of executables
-EOF
+\e[m
+This is my presentation of the Tiny Clear Elf series of executables\n'
+# the 2nd-to-last line is an escape sequence to clear text formatting
 
-wait_for_eof
-
+wait_for_next
 
 for tiny_clear_elf in */clear; do
     "$tiny_clear_elf" # use the current executable to clear the screen
+    # each Tiny Clear ELF is in a folder matching its architecture name
     architecture="$(dirname "$tiny_clear_elf")"
 
 
     arch_specific_logo
-
+    # need this info for proper display and disassembly
     case "$architecture" in
-        amd64|i386) rasm2_arch=x86 endianness=little ;;
-        arm*) rasm2_arch=arm endianness=little ;;
-        mips*) rasm2_arch=mips endianness=little ;;
-        ppc64el) rasm2_arch=ppc endianness=little ;;
-        s390x) rasm2_arch=s390 endianness=big ;;
-        riscv64) rasm2_arch=riscv endianness=little ;;
+           'amd64') rasm2_arch=x86   endianness=little rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
+            'i386') rasm2_arch=x86   endianness=little rasm2_bits=32 ehdr_size=52 phdr_size=32 ;;
+           'armhf') rasm2_arch=arm   endianness=little rasm2_bits=16 ehdr_size=52 phdr_size=32 ;;
+           'armel') rasm2_arch=arm   endianness=little rasm2_bits=32 ehdr_size=52 phdr_size=32 ;;
+           'arm64') rasm2_arch=arm   endianness=little rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
+          'mipsel') rasm2_arch=mips  endianness=little rasm2_bits=32 ehdr_size=52 phdr_size=32 ;;
+        'mips64el') rasm2_arch=mips  endianness=little rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
+         'ppc64el') rasm2_arch=ppc   endianness=little rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
+           's390x') rasm2_arch=s390  endianness=big    rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
+         'riscv64') rasm2_arch=riscv endianness=little rasm2_bits=64 ehdr_size=64 phdr_size=56 ;;
     esac
+    # for armhf, it's technically 32-bits, but because
+    # it uses thumb instructions, rasm2 needs to be told it's 16.
 
-
-    # set some variables that depend on whether it's 64 or 32 bits by parsing the elf file itself
-    case "$(head -c5 "$tiny_clear_elf" | tail -c1)" in
-        $'\x01') bits=32; ehdr_size=52; phdr_size=32  ;;
-        $'\x02') bits=64; ehdr_size=64; phdr_size=56 ;;
-        *) printf 'ERROR: invalid value in elf header.\n' >&2 ; exit 1;;
-    esac
-
-    # print the architecture 48 cells in, so that it doesn't clobber the logo.
-
-    heading 8 'HEXDUMP OF EXECUTABLE'
+    show_heading 8 'HEXDUMP OF EXECUTABLE'
     hexyl "$tiny_clear_elf"
 
-    heading 23 'ELF HEADER OF EXECUTABLE'
+    show_heading 23 'ELF HEADER OF EXECUTABLE'
     hexyl -n"$ehdr_size" "$tiny_clear_elf"
     readelf -h "$tiny_clear_elf"
 
-    read -s
-
-    wait_for_eof
+    wait_for_next
 
     "$tiny_clear_elf"
 
     arch_specific_logo
 
-    heading 8 'PROGRAM HEADER TABLE OF EXECUTABLE'
+    show_heading 8 'PROGRAM HEADER TABLE OF EXECUTABLE'
     hexyl -s"$ehdr_size" -n"$phdr_size" "$tiny_clear_elf"
     readelf -l "$tiny_clear_elf"
 
-    heading 26 'DISSASSEMBLY'
+    show_heading 26 'DISSASSEMBLY'
     case "$endianness" in
         big)
             head -c-10 "$tiny_clear_elf" | tail -c+$((ehdr_size+phdr_size+1)) |\
-                rasm2 -a"$rasm2_arch" -b"$bits" -B -e -d -f - ;;
+                rasm2 -a"$rasm2_arch" -b"$rasm2_bits" -B -e -d -f - ;;
         little)
             head -c-10 "$tiny_clear_elf" | tail -c+$((ehdr_size+phdr_size+1)) |\
-                rasm2 -a"$rasm2_arch" -b"$bits" -B -d -f - ;;
+                rasm2 -a"$rasm2_arch" -b"$rasm2_bits" -B -d -f - ;;
     esac
 
-    heading 38 'HEXDUMP OF LAST 10 BYTES'
+    show_heading 38 'HEXDUMP OF LAST 10 BYTES'
 
     tail -c 10 "$tiny_clear_elf" | hexyl
 
-    heading 42 'HEXDUMP OF OUTPUT'
+    show_heading 42 'HEXDUMP OF OUTPUT'
 
     "$tiny_clear_elf" | hexyl
 
-    read -s
-
+    wait_for_next
 done
+
+cleanup
